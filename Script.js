2c0a3a7515b1b3089c0bd3fb3a0655ad4b588c7c
Sthembiso Mthethwa 22050584 <96879609+SthembisoMthethwa@users.noreply.github.com>
2022-04-11 19:25:48 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -17,9 +17,8 @@ closeBtns.forEach(function(btn){
   };
 });
 
-
 /*js for product Gallery*/
-
+  /*  Model 1 */
 var ProductImg = document.getElementById("ProductImg");
 var SmallImg = document.getElementsByClassName("small-img");
 
@@ -40,3 +39,67 @@ var SmallImg = document.getElementsByClassName("small-img");
     ProductImg.src = SmallImg[3].src;
   }
 
+
+    /*  Model 2 */
+  var ProductImg = document.getElementById("ImageProd");
+  var SmallImg = document.getElementsByClassName("img-small");
+  
+     SmallImg[0].onclick = function()
+    {
+      ProductImg.src = SmallImg[0].src;
+    }
+    SmallImg[1].onclick = function()
+    {
+      ProductImg.src = SmallImg[1].src;
+    }
+    SmallImg[2].onclick = function()
+    {
+      ProductImg.src = SmallImg[2].src;
+    }
+    SmallImg[3].onclick = function()
+    {
+      ProductImg.src = SmallImg[3].src;
+    }
+
+       /*  Model 3 */
+    var ProductImg = document.getElementById("Product");
+    var SmallImg = document.getElementsByClassName("small");
+    
+       SmallImg[0].onclick = function()
+      {
+        ProductImg.src = SmallImg[0].src;
+      }
+      SmallImg[1].onclick = function()
+      {
+        ProductImg.src = SmallImg[1].src;
+      }
+      SmallImg[2].onclick = function()
+      {
+        ProductImg.src = SmallImg[2].src;
+      }
+      SmallImg[3].onclick = function()
+      {
+        ProductImg.src = SmallImg[3].src;
+      }
+
+
+      /*  Model 4 */
+    var ProductImg = document.getElementById("Prod");
+    var SmallImg = document.getElementsByClassName("image-product-for");
+    
+       SmallImg[0].onclick = function()
+      {
+        ProductImg.src = SmallImg[0].src;
+      }
+      SmallImg[1].onclick = function()
+      {
+        ProductImg.src = SmallImg[1].src;
+      }
+      SmallImg[2].onclick = function()
+      {
+        ProductImg.src = SmallImg[2].src;
+      }
+      SmallImg[3].onclick = function()
+      {
+        ProductImg.src = SmallImg[3].src;
+      }
